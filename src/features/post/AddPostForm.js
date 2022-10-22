03e7ddec346f093d7cfd1b0a2bda20742a8db215
unknown <sazwerling@gmail.com>
2022-10-22 12:39:36 -0700

    css and update addPostForm with useRef instead of state and onChange

--- src/features/post/AddPostForm.js
+++ src/features/post/AddPostForm.js
@@ -1,59 +1,53 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { useDispatch } from "react-redux";
 import { nanoid } from "@reduxjs/toolkit";
 import { postAdded } from "./postSlice";
 import React from "react";
 
-
-
 const AddPostForm = () => {
 
-    const [title, setTitle] = useState('')
-    const [content, setContent] = useState('')
+   const titleRef = useRef();
+   const contentRef = useRef();
+   const formRef = useRef();
 
-    const onTitleChange = e => setTitle(e.target.value)
-    console.log(title)
-    
-    const onContentChange = (e) => {setContent(e.target.value)}
 
-    const dispatch = useDispatch()
+   const dispatch = useDispatch();
 
-    const onSavePost = () => {
-        if(title && content){
-            dispatch((
-                postAdded({
-                    id: nanoid(),
-                    title,
-                    content
-                })
-            ))
-            setTitle('')
-            setContent('') 
-        }  
-    }
+   const onSavePost = () => {
+      if (titleRef && contentRef) {
+         dispatch(
+            postAdded({
+               id: nanoid(),
+               title: titleRef.current.value,
+               content: contentRef.current.value,
+            })
+         );
+         formRef.current.reset();
+      }
+   };
 
    return (
       <section>
          <h2>Add A New Post</h2>
-         <form>
-         <label htmlFor="postTitle">Post Title:</label>
-                <input
-                    type="text"
-                    id="postTitle"
-                    name="postTitle"
-                    value={title}
-                    onChange={onTitleChange}
-                />
+         <form ref={formRef}>
+            <label htmlFor="postTitle">Post Title:</label>
+            <input
+               type="text"
+               id="postTitle"
+               name="postTitle"
+               ref={titleRef}
+            />
             <label htmlFor="postContent">Content:</label>
             <textarea
                name="postContent"
                id="postContent"
-               value={content}
-               onChange={onContentChange}
-               cols="30"
-               rows="10"
+               ref={contentRef}
+            //    cols="30"
+            //    rows="10"
             />
-            <button type="button" onClick={onSavePost}>Save Post</button>
+            <button type="button" onClick={onSavePost}>
+               Save Post
+            </button>
          </form>
       </section>
    );
