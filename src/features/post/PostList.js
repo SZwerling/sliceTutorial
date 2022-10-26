7dce07cc9a9e7b06e2caad641a1f41292bae1cb5
unknown <sazwerling@gmail.com>
2022-10-26 11:20:55 -0700

    order comments so newest first

--- src/features/post/PostList.js
+++ src/features/post/PostList.js
@@ -6,8 +6,10 @@ import React from "react";
 
 const PostList = () => {
    const posts = useSelector(selectAllPosts); //this name has nothing to do with store.js//It's referencing the slice
+   // BELOW .slice returns a shallow copy of the arr // localeCompare returns a number in regards to sort order
+   const sortedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-   const renderedPosts = posts.map((post) => {
+   const renderedPosts = sortedPosts.map((post) => {
       return (
          <article key={post.id}>
             <h3>{post.title}</h3>
